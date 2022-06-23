75cebb8ecf67dc8f0d4391668d7f18c6285ba11d
CFX96 <frantisek.mrazek06@seznam.cz>
2022-06-23 17:22:48 +0200

    added CircleXCirecleCrossection class

---
+++ src/shapes/CircleXCircleCrossections.java
@@ -0,0 +1,72 @@
+package shapes;
+
+import java.awt.*;
+import java.awt.geom.Ellipse2D;
+import java.awt.image.BufferedImage;
+
+public class CircleXCircleCrossections {
+    public Float[] centre1;
+    public Float[] centre2;
+
+    public Float[] diameter1;
+    public Float[] diameter2;
+
+    public Float[] position1 = new Float[]{null, null};
+    public Float[] position2 = new Float[]{null, null};
+
+    public float renderDiameter;
+    public Color renderColor;
+
+    public boolean exists;
+
+
+
+    public CircleXCircleCrossections(Float[] centre1, Float[] diameter1, Float[] centre2, Float[] diameter2, float renderDiameter, Color renderColor){
+        this.centre1 = centre1;
+        this.centre2 = centre2;
+        this.diameter1 = diameter1;
+        this.diameter2 = diameter2;
+        this.renderColor = renderColor;
+        this.renderDiameter = renderDiameter;
+    }
+
+    public void update(){
+        if(diameter1[0] == null | diameter1[1] == null | diameter2[0] == null | diameter2[1] == null | centre1[0] == null | centre1[1] == null | centre2[0] == null | centre2[1] == null){exists = false;}else{exists = true;}
+        if(exists){
+            float d = (float) Math.sqrt((Math.abs(centre1[0] - centre2[0])*Math.abs(centre1[0] - centre2[0]))+(Math.abs(centre1[1] - centre2[1])*Math.abs(centre1[1] - centre2[1])));
+
+            if(d > diameter1[0] + diameter2[0]){exists = false; position1[0] = null; position1[1] = null; position2[0] = null; position2[1] = null;}
+            else{
+                float a = (diameter1[0]*diameter1[0] - diameter2[0]*diameter2[0] + d*d)/2*d;
+                float h = (float) Math.sqrt(diameter1[0]*diameter1[0]-a*a);
+
+                float[] p2 = new float[]{centre1[0]+a*(centre2[0]-centre1[0])/d, centre1[1]+a*(centre2[1]-centre1[1])/d};
+
+                position1[0] = p2[0]+h*(centre2[1]-centre1[1])/d;
+                position1[1] = p2[1]+h*(centre2[0]-centre1[0])/d;
+
+                position1[0] = p2[0]-h*(centre2[1]-centre1[1])/d;
+                position2[1] = p2[1]-h*(centre2[0]-centre1[0])/d;
+
+                exists = true;
+            }
+
+        }
+    }
+
+    public BufferedImage renderOnImage(BufferedImage image){
+        if(position1[0] == null | position1[1] == null | position2[0] == null | position2[1] == null){exists = false;}else{exists = true;}
+        if (exists){
+            Graphics2D g2d = image.createGraphics();
+            g2d.setPaint(renderColor);
+            Ellipse2D.Double circle1 = new Ellipse2D.Double(position1[0] - (renderDiameter / 2f), position1[1] - (renderDiameter / 2f), renderDiameter, renderDiameter);
+            g2d.fill(circle1);
+            Ellipse2D.Double circle2 = new Ellipse2D.Double(position2[0] - (renderDiameter / 2f), position2[1] - (renderDiameter / 2f), renderDiameter, renderDiameter);
+            g2d.fill(circle2);
+            g2d.dispose();
+
+        }
+
+        return image;
+    }
+}
